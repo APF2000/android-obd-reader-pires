9648f8135009fc31d7338b7e8b126dc1ff38eb2b
APF2000 <arthurpfonseca@gmail.com>
2023-10-28 17:24:33 -0300

    added gps + tried gradle sync

--- src/main/java/com/github/pires/obd/reader/activity/MainActivity.java
+++ src/main/java/com/github/pires/obd/reader/activity/MainActivity.java
@@ -104,6 +104,17 @@ import com.android.volley.Response.Listener;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
+
+
 // Some code taken from https://github.com/barbeau/gpstest
 
 @ContentView(R.layout.main)
@@ -140,6 +151,17 @@ public class MainActivity extends RoboActivity implements ObdProgressListener, L
     /// the trip log
     private TripLog triplog;
     private TripRecord currentTrip;
+
+    private String TAG = "MainActivity";
+    private int FASTEST_INTERVAL = 8 * 1000; // 8 SECOND
+    private int UPDATE_INTERVAL = 2000; // 2 SECOND
+    private int FINE_LOCATION_REQUEST = 888;
+    private Toast toast;
+    private LocationRequest locationRequest;
+
+    private TextView tvLocationDetails;
+    private LinearLayout mainLayout;
+
 
 
     @Override
@@ -231,41 +253,41 @@ public class MainActivity extends RoboActivity implements ObdProgressListener, L
         }
     };
 
-    @InjectView(R.id.acceleration_text)
-    private TextView acceleration;
-    private final SensorEventListener accelerationListener = new SensorEventListener() {
-        public void onSensorChanged(SensorEvent event) {
-            /*
-            // alpha is calculated as t / (t + dT)
-            // with t, the low-pass filter's time-constant
-            // and dT, the event delivery rate
-
-            final float alpha = 0.8;
-
-            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-            linear_acceleration[0] = event.values[0] - gravity[0];
-            linear_acceleration[1] = event.values[1] - gravity[1];
-            linear_acceleration[2] = event.values[2] - gravity[2];
-        }
-
-*/
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            DecimalFormat df = new DecimalFormat("#0.0");
-            String acc = df.format(x);
-
-            updateTextView(acceleration, acc);
-
-        }
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // do nothing
-        }
-    };
+//    @InjectView(R.id.acceleration_text)
+//    private TextView acceleration;
+//    private final SensorEventListener accelerationListener = new SensorEventListener() {
+//        public void onSensorChanged(SensorEvent event) {
+//            /*
+//            // alpha is calculated as t / (t + dT)
+//            // with t, the low-pass filter's time-constant
+//            // and dT, the event delivery rate
+//
+//            final float alpha = 0.8;
+//
+//            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+//            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+//            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+//
+//            linear_acceleration[0] = event.values[0] - gravity[0];
+//            linear_acceleration[1] = event.values[1] - gravity[1];
+//            linear_acceleration[2] = event.values[2] - gravity[2];
+//        }
+//
+//*/
+//            float x = event.values[0];
+//            float y = event.values[1];
+//            float z = event.values[2];
+//
+//            DecimalFormat df = new DecimalFormat("#0.0");
+//            String acc = df.format(x);
+//
+//            updateTextView(acceleration, acc);
+//
+//        }
+//        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//            // do nothing
+//        }
+//    };
 
 
     @InjectView(R.id.BT_STATUS)
@@ -915,5 +937,151 @@ public class MainActivity extends RoboActivity implements ObdProgressListener, L
             return null;
         }
 
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initViewsAndListener();
+        if (checkPermissions()) {
+            initLocationUpdate();
+        }
+
+    }
+
+    private void initViewsAndListener() {
+        toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        tvLocationDetails=findViewById(R.id.tvLocationDetails);
+        mainLayout=findViewById(R.id.mainLayout);
+        findViewById(R.id.btnGetLocation).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkPermissions()) {
+                    initLocationUpdate();
+                }
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    //Start Location update as define intervals
+    private void initLocationUpdate() {
+
+        // Check API revision for New Location Update
+        //https://developers.google.com/android/guides/releases#june_2017_-_version_110
+
+        //init location request to start retrieving location update
+        locationRequest = new LocationRequest();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval( UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+
+        //Create LocationSettingRequest object using locationRequest
+        LocationSettingsRequest.Builder locationSettingBuilder =new LocationSettingsRequest.Builder();
+        locationSettingBuilder.addLocationRequest(locationRequest);
+        LocationSettingsRequest locationSetting = locationSettingBuilder.build();
+
+        //Need to check whether location settings are satisfied
+        SettingsClient settingsClient= LocationServices.getSettingsClient(this);
+        settingsClient.checkLocationSettings(locationSetting);
+        //More info :  // https://developers.google.com/android/reference/com/google/android/gms/location/SettingsClient
+
+
+        // new Google API SDK v11 uses getFusedLocationProviderClient(this)
+        FusedLocationProviderClient fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, new LocationCallback(){
+
+                    @Override
+                    public void onLocationResult(LocationResult locationResult) {
+                        //super.onLocationResult(locationResult);
+                        if (locationResult != null) {
+                            onLocationChanged(locationResult.getLastLocation());
+                        }
+                    }
+
+                    @Override
+                    public void onLocationAvailability(LocationAvailability locationAvailability) {
+                        super.onLocationAvailability(locationAvailability);
+                    }
+                },
+                Looper.myLooper());
+
+    }
+    private void onLocationChanged(Location location) {
+        // New location has now been determined
+        String msg = "Updated Location: " +
+                java.lang.Double.toString(location.getLatitude()) + "," +
+                java.lang.Double.toString(location.getLongitude());
+        tvLocationDetails.setText(msg);
+        toast.setText(msg);
+        toast.show();
+    }
+
+
+    private boolean checkPermissions(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            requestPermissions();
+            return false;
+        }
+    }
+
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                FINE_LOCATION_REQUEST);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == FINE_LOCATION_REQUEST) {
+            // Received permission result for Location permission.
+            Log.i(TAG, "Received response for Location permission request.");
+
+            // Check if the only required permission has been granted
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission has been granted, preview can be displayed
+                Log.i(TAG, "Location permission has now been granted. Now call initLocationUpdate");
+                initLocationUpdate();
+            } else {
+                Snackbar.make(mainLayout, R.string.rational_location_permission,
+                                Snackbar.LENGTH_INDEFINITE)
+                        .setAction(getString(R.string.ok), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                requestPermissions();
+                            }
+                        }).show();
+
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+
+    @Override
+    protected void onStop() {
+        if (toast != null) {
+            toast.cancel();
+        }
+        super.onStop();
+    }
+
+
+    @Override
+    protected void onPause() {
+        if (toast != null) {
+            toast.cancel();
+        }
+        super.onPause();
+        super.onPause();
     }
 }
